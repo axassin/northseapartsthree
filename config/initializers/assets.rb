cffe94ba47c59197a47edc685f30a0e14d03d464
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-10-07 14:55:39 +0800

    surewin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,20 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+=begin
+[DevelopmentController, HomeController].each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+end
+=end
+
+js_prefix    = 'app/assets/javascripts/'
+style_prefix = 'app/assets/stylesheets/'
+image_prefix = 'app/assets/images/'
+
+javascripts = Dir["#{js_prefix}**/*.js"].map      { |x| x.gsub(js_prefix,    '') }
+css         = Dir["#{style_prefix}**/*.css"].map  { |x| x.gsub(style_prefix, '') }
+scss        = Dir["#{style_prefix}**/*.scss"].map { |x| x.gsub(style_prefix, '') }
+image       = Dir["#{image_prefix}**/*.*"].map { |x| x.gsub(image_prefix, '') }
+
+Rails.application.config.assets.precompile = (javascripts + css + scss + image)
