b21aac84b1a71efad70deaa51ca185287936bbf3
joojieman <jamespatrickpe@gmail.com>
2016-10-20 07:47:07 +0800

    dfs surewin

--- app/controllers/enterprise/general_management/vehicles_controller.rb
+++ app/controllers/enterprise/general_management/vehicles_controller.rb
@@ -3,7 +3,7 @@ class Enterprise::GeneralManagement::VehiclesController < ApplicationController
   layout 'generic_resource'
 
   def index
-    setup_table(Vehicle, 'fa fa-bus', nil, 'Transportation Assets')
+    setup_table(Vehicle, 'fa fa-bus', nil, 'Transportation Assets', ['primary_image'] )
   end
 
 end
