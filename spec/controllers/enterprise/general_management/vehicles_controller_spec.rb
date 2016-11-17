8c29203fceafb6c26ad6af98eb786d0b4c087db9
joojieman <jamespatrickpe@gmail.com>
2016-11-18 07:13:38 +0800

    dafdfa

--- spec/controllers/enterprise/general_management/vehicles_controller_spec.rb
+++ spec/controllers/enterprise/general_management/vehicles_controller_spec.rb
@@ -1,33 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Enterprise::GeneralManagement::ContactsController, type: :controller do
+RSpec.describe Enterprise::GeneralManagement::VehiclesController, type: :controller do
 
-  describe 'GET index' do
-
-    it 'response success' do
-      get :index
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders template' do
-      get :index
-      expect(response).to render_template('index')
-    end
-
-    it 'loads initial data' do
-      vehicle_array = []
-
-      10.times {
-        vehicle_array.push(FactoryGirl.create :vehicle)
-      }
-      # vehicle_01 = FactoryGirl.create :vehicle
-
-      get :index
-      expect(assigns(:result_set)).to match_array(vehicle_array)
-
-    end
-
-  end
+  it_behaves_like 'generic_resource_controller', 'vehicle'
 
 end
