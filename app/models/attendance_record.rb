28ed78bb8e5b920fb7b3eec64deefde4391eedbf
joojieman <jamespatrickpe@gmail.com>
2017-02-24 07:46:16 +0800

    dasd

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -16,10 +16,21 @@ class AttendanceRecord < ApplicationRecord
   end
 
 
-  def self.point_hour(point_date, point_time_hour)
-    AttendanceRecord.where(implemented_at: point_date).each do |attendance_record|
-
+  def self.point_hour(point_date, point_hour, employee_id)
+    time_hit = 'n.a.'
+    AttendanceRecord.where(implemented_at: point_date, employee_id: employee_id).each do |attendance_record|
+      time_in = attendance_record.time_in
+      time_out = attendance_record.time_out
+      if time_in.between?(point_hour, point_hour + 1.hour)
+        time_hit = time_in
+      elsif time_out.between?(point_hour, point_hour + 1.hour)
+        time_hit = time_out
+      end
+      puts time_in
+      puts point_hour
+      puts '-------------'
     end
+    time_hit
   end
 
 end
