73e6daa2092a6e8acbc4e744befc0f1284d2a3f4
James Patrick G. Pe <jamespatrickpe@gmail.com>
2017-03-20 18:05:36 +0800

    dasdfs

--- app/controllers/enterprise/operations/database_schema_translator_controller.rb
+++ app/controllers/enterprise/operations/database_schema_translator_controller.rb
@@ -19,31 +19,45 @@ class Enterprise::Operations::DatabaseSchemaTranslatorController < GenericDashbo
 
     system_account_sheet.each(id: 'id', name: 'name', remark: 'remark') do |system_account_hash|
 
-
       system_account = SystemAccount.new()
       system_account.name = system_account_hash[:name]
       system_account.description = system_account_hash[:remark]
-      system_account.account_type = 'INDIVIDUAL'
+      system_account.account_type = 'GROUP'
       system_account.save!
 
       basis_id = system_account_hash[:id]
       system_account_id = system_account.id
 
-=begin
+      contact_detail = ContactDetail.new()
+      contact_detail.label = 'main'
+      contact_detail.contactable_id = system_account.id
+      contact_detail.contactable_type = 'SystemAccount'
+      contact_detail.save!
+
+      contact_detail_id = contact_detail.id
+
       telephones_sheet.each(digits: 'digits', remark: 'remark', telephonable_type: 'telephonable_type', telephonable_id: 'telephonable_id') do |telephones_sheet|
         if telephones_sheet[:telephonable_type] == 'SystemAccount' && telephones_sheet[:telephonable_id] == basis_id
-          puts telephones_sheet[:digits]
-          puts telephones_sheet[:remark]
+          telephone_number = TelephoneNumber.new
+          telephone_number.contact_detail_id = contact_detail_id
+          telephone_number.remark = telephones_sheet[:remark]
+          telephone_number.digits = telephones_sheet[:digits]
+          telephone_number.save
         end
       end
 
       addresses_sheet.each(remark: 'remark', addressable_type: 'addressable_type', addressable_id: 'addressable_id') do |addresses_sheet|
         if addresses_sheet[:addressable_type] == 'SystemAccount' && addresses_sheet[:addressable_id] == basis_id
-          puts telephones_sheet[:digits]
-          puts telephones_sheet[:remark]
+          location = Location.new
+          location.address = addresses_sheet[:remark]
+          location.contact_detail_id = contact_detail_id
+          location.longitude = 0.00000000
+          location.latitude = 0.00000000
+          location.save
         end
       end
-=end
+
+      puts 'processing : ' + system_account.name
 
     end
 
