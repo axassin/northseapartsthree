d4c6731915e2e8ed56de777bf6f3aee548256c87
klstar072 <karyn072@gmail.com>
2017-04-26 22:31:59 +0800

    fhtyr

--- app/models/storage_unit.rb
+++ app/models/storage_unit.rb
@@ -10,11 +10,21 @@ class StorageUnit < ApplicationRecord
               @@routes.enterprise_operations_storage_management_storage_units_path,
               Enterprise::Operations::StorageManagement::StorageUnitsController)
   
-  
   searchable_string(:code)
 
-  def complete_code
-
+  def complete_code    
+    sample_chain = ''
+    delimiter=':'
+    StorageUnit.where(code: code).each do |storage_unit|
+      storage_unit.ancestors.each do |ancestor|
+        unless sample_chain.present?
+          sample_chain = ancestor.code.to_s  
+        else
+          sample_chain = sample_chain + delimiter + ancestor.code.to_s  
+        end 
+      end
+    end
+    sample_chain
   end
 
   def parent_code
