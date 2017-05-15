e70353a78ded93900d1a82082ad157c5ccabe295
klstar072 <karyn072@gmail.com>
2017-05-15 14:33:05 +0800

    anc mrg

--- app/models/storage_unit.rb
+++ app/models/storage_unit.rb
@@ -10,7 +10,6 @@ class StorageUnit < ApplicationRecord
               @@routes.enterprise_operations_storage_management_storage_units_path,
               Enterprise::Operations::StorageManagement::StorageUnitsController)
 
-<<<<<<< HEAD
   searchable_string(:code)
 
   def complete_code    
@@ -26,12 +25,6 @@ class StorageUnit < ApplicationRecord
       end
     end
     sample_chain
-=======
-  def complete_code
-    'dfasdnfaskldjfhdklfh '
-    puts '----------------- --- ---'
-    puts 'dfasdfas'
->>>>>>> 2933f22628a08817e60f9a403095c372fbff96b4
   end
 
   def parent_code
