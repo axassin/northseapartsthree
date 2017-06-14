64f114ff827ad57cce0620390a0e69f4701fadab
joojieman <jamespatrickpe@gmail.com>
2017-06-14 12:58:22 +0800

    fasdfasd

--- app/controllers/generic_report_controller.rb
+++ app/controllers/generic_report_controller.rb
@@ -9,7 +9,7 @@ class GenericReportController < ApplicationController
   before_action :setup_controller
   before_action :authenticate_user!
 
-  def setup_variables(main_resource_path, parent_controller_path, title_heading = 'generic_field')
+  def setup_variables(main_resource_path, parent_controller_path, title_heading = 'Generic Report')
     @main_resource_path = main_resource_path
     @parent_controller_path = parent_controller_path
     @title_heading = title_heading.upcase
